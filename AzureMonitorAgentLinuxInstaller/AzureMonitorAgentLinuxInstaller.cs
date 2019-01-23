fe68ce0e264b36dc33e0633803f68421280965ca
Hazem Elshabini <v-hazeme@microsoft.com>
2019-01-23 14:04:41 +0300

    ssh basics

--- AzureMonitorAgentLinuxInstaller/AzureMonitorAgentLinuxInstaller.cs
+++ AzureMonitorAgentLinuxInstaller/AzureMonitorAgentLinuxInstaller.cs
@@ -1,13 +1,16 @@
 ﻿using Newtonsoft.Json;
+using Renci.SshNet;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,7 +18,7 @@ namespace AzureMonitorAgentLinuxInstaller
 {
     public partial class AzureMonitorAgentLinuxInstaller : Form
     {
-        private BindingSource bindingSource1;
+        private BindingSource serverlistDataSource;
 
         public AzureMonitorAgentLinuxInstaller()
         {
@@ -25,8 +28,8 @@ namespace AzureMonitorAgentLinuxInstaller
 
         private void InitializeServersList()
         {
-            bindingSource1 = new BindingSource();
-            bindingSource1.Add(new ServerListItem
+            serverlistDataSource = new BindingSource();
+            serverlistDataSource.Add(new ServerListItem
             {
                 Status = ServerListItemStatus.NotStarted,
                 ServerName = "autodeploytestvm1",
@@ -38,7 +41,7 @@ namespace AzureMonitorAgentLinuxInstaller
             });
             gvServersList.AutoGenerateColumns = false;
             gvServersList.AutoSize = true;
-            gvServersList.DataSource = bindingSource1;
+            gvServersList.DataSource = serverlistDataSource;
             
             // Initialize and add a Progress column
             DataGridViewColumn column = new DataGridViewImageColumn();
@@ -162,9 +165,58 @@ namespace AzureMonitorAgentLinuxInstaller
 
         private void btnDeploy_Click(object sender, EventArgs e)
         {
-            DownloadAgents();
+            //DownloadAgents();
             
             //Loop on servers in the grid view
+            foreach (var item in serverlistDataSource)
+            {
+                ServerListItem server = item as ServerListItem;
+                var connectionInfo = new ConnectionInfo(server.IPAddress,
+                                        server.Username,
+                                        new PasswordAuthenticationMethod(server.Username, server.Password));
+                using (var client = new SshClient(connectionInfo))
+                {
+                    client.Connect();
+
+                    IDictionary<Renci.SshNet.Common.TerminalModes, uint> termkvp = new Dictionary<Renci.SshNet.Common.TerminalModes, uint>();
+                    termkvp.Add(Renci.SshNet.Common.TerminalModes.ECHO, 53);
+                    ShellStream shellStream = client.CreateShellStream("xterm", 80, 24, 800, 600, 1024, termkvp);
+
+                    //var cmd = client.CreateCommand("pwd; pwd;");
+                    shellStream.WriteLine("sudo pwd;");
+                    string rep = shellStream.Expect(new Regex(@"([$#>:])")); //expect password or user prompt
+
+                    server.Log += rep;
+
+                    //check to send password
+                    if (rep.Contains(":"))
+                    {
+                        //send password
+                        shellStream.WriteLine(server.Password);
+                        rep = shellStream.Expect(new Regex(@"[$#>]")); //expect user or root prompt
+                        server.Log += rep;
+                    }
+
+                    client.Disconnect();
+
+                    //var result = cmd.BeginExecute();
+
+                    //using (var reader =
+                    //           new StreamReader(cmd.OutputStream, Encoding.UTF8, true, 1024, true))
+                    //{
+                    //    while (!result.IsCompleted || !reader.EndOfStream)
+                    //    {
+                    //        string line = reader.ReadLine();
+                    //        if (line != null)
+                    //        {
+                    //            server.Log += (line + Environment.NewLine);
+                    //        }
+                    //    }
+                    //}
+
+                    //cmd.EndExecute(result);
+                }
+            }
             //Check whether server is x64 or x86
             //Install prerequisites using local yum repo for rpm or apt for debian (not important now as most servers would already have those files)
             //SCP the appropriate file on the server
